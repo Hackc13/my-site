29984b7401443400bbe55f9ce508fec3bd6232b4
Hackc13 <124883098+Hackc13@users.noreply.github.com>
2026-03-09 04:29:05 +0000

    Update firebase.js

--- chat/firebase.js
+++ chat/firebase.js
@@ -1,14 +1,14 @@
 // chat/firebase.js
 // Replace with your Firebase config
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCz4PpJvoQekaeqD5KLakwlDtN5vlBQhOk",
+  authDomain: "chat-c51d9.firebaseapp.com",
+  projectId: "chat-c51d9",
+  storageBucket: "chat-c51d9.firebasestorage.app",
+  messagingSenderId: "571544571750",
+  appId: "1:571544571750:web:eee65ca1c1a3852a0a82a7"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
